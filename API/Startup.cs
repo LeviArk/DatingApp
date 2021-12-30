e5bdabffa14671fa4ccdafafdadd82cc31f72b1f
Ark <digitalshadow2.0@gmail.com>
2021-12-30 01:15:05 -0500

    Eos 7

--- API/Startup.cs
+++ API/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.OpenApi.Models;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using API.Extensions;
+using API.Middleware;
 
 namespace API;
 public class Startup
@@ -36,10 +37,11 @@ public class Startup
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
+    {   
+        app.UseMiddleware<ExceptionMiddleware>();
+
         if (env.IsDevelopment())
         {
-            app.UseDeveloperExceptionPage();
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
         }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -25,7 +25,6 @@ export class NavComponent implements OnInit {
       this.toastr.success("Welcome back." );
     }, error=> {
       console.log(error);
-      this.toastr.error(error.error);
     });
   }
 
