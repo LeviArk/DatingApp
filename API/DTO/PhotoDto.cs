43bf035e6b6e837c4316ef6b88d0ee54f5e23b70
Ark <digitalshadow2.0@gmail.com>
2021-12-30 16:34:02 -0500

    Eos 8

---
+++ API/DTO/PhotoDto.cs
@@ -0,0 +1,10 @@
+namespace API.DTO
+{
+    public class PhotoDto
+    {
+        public int Id { get; set; }
+        public string url { get; set; }
+
+        public bool isMain { get; set; }
+    }
+}
